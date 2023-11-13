065886475a1207a7b2b186f2f419511737518c12
testmapper <67954166+testmapper-official@users.noreply.github.com>
2023-11-13 10:10:05 +0400

    lab #3 fix

--- Lab #3/TASK_7_NO_JOIN.sql
+++ Lab #3/TASK_7_NO_JOIN.sql
@@ -1,8 +1,9 @@
 -- выбирает членов клуба рекомендованных к вступлению другими членам клуба и людей рекомендавших их в алфавитном порядке БЕЗ использования JOIN.
 SELECT DISTINCT
+      mem1.memid as "ID",
       CONCAT(mem1.surname, " ", mem1.firstname) as 'Участник',
-      IF(mem1.recommendedby IS NULL, "NULL", CONCAT(mem2.surname, " ", mem2.firstname)) as 'Рекомендован'
-  FROM cd.members as mem1, cd.members mem2
-  WHERE (mem1.recommendedby = mem2.memid OR mem1.recommendedby IS NULL)
-        and mem1.surname NOT LIKE "GUEST"
-  ORDER BY Участник;
+      IF(mem1.recommendedby IS NULL, NULL, CONCAT(mem2.surname, " ", mem2.firstname)) as 'Рекомендован'
+FROM cd.members as mem1, cd.members mem2
+WHERE (mem1.recommendedby = mem2.memid OR mem1.recommendedby IS NULL)
+      and mem1.surname NOT LIKE "GUEST"
+ORDER BY Участник;
