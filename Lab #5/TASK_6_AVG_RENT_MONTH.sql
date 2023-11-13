c57890a42b0b0028443a5812618d20d2cb745cd0
testmapper <67954166+testmapper-official@users.noreply.github.com>
2023-11-13 09:55:09 +0400

    fix comment

--- Lab #5/TASK_6_AVG_RENT_MONTH.sql
+++ Lab #5/TASK_6_AVG_RENT_MONTH.sql
@@ -1,4 +1,4 @@
--- Выводит кол-во аренд каждого объекта клуба в месяц за 2012 год.
+-- Выводит кол-во бронирований каждого объекта клуба в месяц за 2012 год.
 SELECT facility, COUNT(book.bookid) / COUNT(DISTINCT MONTH(book.starttime)) as avg_rent_month
 FROM cd.facilities as fac
 LEFT JOIN cd.bookings as book ON book.facid = fac.facid
