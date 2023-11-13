ccf22615ae0d8aafaca53bc0d8b7656372a44713
testmapper <67954166+testmapper-official@users.noreply.github.com>
2023-11-13 10:36:28 +0400

    lab #4 fix

--- Lab #4/TASK_3_ANOTHER_SPA.sql
+++ Lab #4/TASK_3_ANOTHER_SPA.sql
@@ -1,12 +1,10 @@
 /*
-Добавьте еще одно spa с такими же характеристиками 
+Добавьте еще одно spa с такими же характеристиками
 как в задании 1, сгенерировав новое ID.
 */
 
 USE cd;
 
-SELECT @maxid := MAX(facid)
-  FROM facilities;
-
 INSERT facilities(facid, facility, membercost, guestcost, initialoutlay, monthlymaintenance)
-  VALUES (@maxid + 1, "spa", 20, 30, 100000, 800);
+SELECT MAX(facid) + 1, "spa", 20, 30, 100000, 800
+FROM facilities;
