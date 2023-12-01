0268b89fc2f322d0bdb2090bc57afe1f37235cd5
testmapper <67954166+testmapper-official@users.noreply.github.com>
2023-12-02 00:52:56 +0400

    13

--- Lab #7/TASK_1_getRentCost.sql
+++ Lab #7/TASK_1_getRentCost.sql
@@ -1,22 +1,21 @@
 -- Функция расчета стоимости аренды записи бронирования.
-
 USE cd;
 
 DELIMITER $$
 
 DROP FUNCTION IF EXISTS getRentCost //
 CREATE FUNCTION getRentCost(memid INT, facid INT, slots INT) RETURNS INT
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
+    READS SQL DATA
+    NOT DETERMINISTIC
+BEGIN
     DECLARE income INT;
     SET income = (SELECT IF(memid = 0, guestcost, membercost) * slots
-                   FROM facilities
-                   WHERE facid = facilities.facid);
+    FROM facilities
+    WHERE facid = facilities.facid);
     RETURN income;
-  END $$
+END $$
 
 DELIMITER ;
 
 SELECT getRentCost(memid, facid, slots)
-  FROM bookings;
+FROM bookings;
