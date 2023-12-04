9a6b2127a045d7936b29124cf82e127a521cdc40
testmapper <67954166+testmapper-official@users.noreply.github.com>
2023-12-04 22:25:12 +0400

    fix 1-2 lab 7

--- Lab #7/TASK_2_PAYMENTS_TABLE.sql
+++ Lab #7/TASK_2_PAYMENTS_TABLE.sql
@@ -8,56 +8,43 @@ CREATE TABLE IF NOT EXISTS payments (
     payment DECIMAL DEFAULT 0,
     FOREIGN KEY (bookid) REFERENCES bookings (bookid)
 );
+-- Редактирование таблицы bookings если она еще не имеет этих изменений.
+ALTER TABLE bookings ADD payed TINYINT DEFAULT 0;
 
 DELIMITER $$
-
--- Редактирование таблицы bookings если она еще не имеет этих изменений.
-DROP PROCEDURE IF EXISTS createPayedProperty $$
-CREATE PROCEDURE createPayedProperty()
-BEGIN
-    IF ( NOT EXISTS (
-            SELECT * FROM information_schema.COLUMNS
-            WHERE TABLE_SCHEMA = 'cd' AND TABLE_NAME = 'bookings' AND COLUMN_NAME = 'payed' 
-        ) ) THEN
-        ALTER TABLE bookings ADD payed TINYINT DEFAULT 0;
-    END IF;
-END $$
-
-CALL createPayedProperty() $$
 
 -- Событие на запрет удаления оплаченного бронирования.
 DROP TRIGGER IF EXISTS onBookingDelete $$
 
 CREATE TRIGGER onBookingDelete BEFORE DELETE ON bookings FOR EACH ROW
 BEGIN
-    IF (    EXISTS (
-                SELECT 1
-                FROM payments as pay
-                WHERE pay.bookid = OLD.bookid
-            )
-        ) THEN
+    IF (OLD.payed = 1) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "You can't delete already paid booking";
     END IF;
 END $$
 
 -- Событие при изменении статуса оплаты в bookings.
 DROP TRIGGER IF EXISTS onBookingPay $$
-
 CREATE TRIGGER onBookingPay AFTER UPDATE ON bookings FOR EACH ROW
 BEGIN
--- Нашел, что MySQL страдает странной болезнью при сравнении OLD и NEW (<> или != вообще не работает, а <=> срабатывает на 2й раз).
--- И судя по всему это еще было в прошлой версии MySQL (5.*), но в 8.* по-прежнему все плохо, хоть и не так сильно как раньше.
+    -- Если бронь изменила свой статус оплаты, то удаляет\добавляет оплату (в ином случае делает ничего).
+    CASE
+        WHEN NEW.payed = OLD.payed THEN BEGIN END;
+        WHEN NEW.payed = 1 THEN
+            INSERT INTO payments(bookid, payment)
+            VALUES(NEW.bookid, getRentCost(NEW.memid, NEW.facid, NEW.slots));
+        WHEN NEW.payed = 0 THEN
+            DELETE FROM payments as pay WHERE pay.bookid = NEW.bookid;
+    END CASE;
+END $$
 
--- Если бронь изменила свой статус оплаты, то удаляет\добавляет оплату (в ином случае делает ничего).
-    IF NEW.payed <=> OLD.payed THEN
-        CASE
-            WHEN NEW.payed = 1 THEN
-                INSERT INTO payments(bookid, payment)
-                SELECT NEW.bookid, getRentCost(memid, facid, slots)
-                FROM bookings as book WHERE book.bookid = NEW.bookid LIMIT 1;
-            WHEN NEW.payed = 0 THEN
-                DELETE FROM payments as pay WHERE pay.bookid = NEW.bookid;
-        END CASE;
+DROP TRIGGER IF EXISTS onBookingCreatePayed $$
+CREATE TRIGGER onBookingCreatePayed AFTER INSERT ON bookings FOR EACH ROW
+BEGIN
+    -- Если бронь уже оплачена при регистрации (payed = 1), добавляет оплату.
+    IF NEW.payed = 1 THEN
+        INSERT INTO payments(bookid, payment)
+        VALUES(NEW.bookid, getRentCost(NEW.memid, NEW.facid, NEW.slots));
     END IF;
 END $$
 
