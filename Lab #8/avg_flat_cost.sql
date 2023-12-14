1d5f7573296ca26df0cba8b165513ef5e0303ba5
testmapper <67954166+testmapper-official@users.noreply.github.com>
2023-12-15 00:56:50 +0400

    update lab 8

--- Lab #8/avg_flat_cost.sql
+++ Lab #8/avg_flat_cost.sql
@@ -1,6 +1,6 @@
 -- Список средних стоимостей квартир по количеству комнат.
 USE estates;
 CREATE VIEW avg_flat_cost AS
-    SELECT rooms, AVG(cost) as avg_cost
+    SELECT rooms, ROUND(AVG(cost), 2) as avg_cost
     FROM Apartment
     GROUP BY rooms;
