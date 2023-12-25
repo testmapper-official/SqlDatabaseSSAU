24dd068f14925852b64f2cf7fd9a5675f5e0e5dc
testmapper <67954166+testmapper-official@users.noreply.github.com>
2023-12-25 12:58:22 +0400

    update lab #7

--- Lab #7/TASK_3_getPayback.sql
+++ Lab #7/TASK_3_getPayback.sql
@@ -4,22 +4,20 @@ USE cd;
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS getPayback $$
-CREATE PROCEDURE getPayback(whichDate DATE)
+CREATE PROCEDURE getPayback(facid INT, m INT, y INT)
     READS SQL DATA
     NOT DETERMINISTIC
 BEGIN
     SELECT
         b.facid
     ,   f.facility
-    ,   SUM(p.payment) - f.monthlymaintenance AS income
+    ,   f.initialoutlay / (SUM(p.payment) - f.monthlymaintenance) AS occupacy
     FROM bookings AS b
     JOIN payments AS p ON b.bookid = p.bookid
     JOIN facilities AS f ON b.facid = f.facid
-    WHERE DATE_FORMAT(starttime, '%y %m') = DATE_FORMAT(whichDate, '%y %m')
+    WHERE facid = b.facid AND MONTH(b.starttime) = m AND YEAR(b.starttime) = y
     GROUP BY b.facid
     ORDER BY b.facid;
 END $$
 
-DELIMITER ;
-
-CALL getPayback('2012-07-03');
+CALL getPayback(4, MONTH('2012-07-03'), YEAR('2012-07-03'));
--- Lab #7/TASK_4_getDiffPayback.sql
+++ Lab #7/TASK_4_getDiffPayback.sql
@@ -1,35 +1,62 @@
--- Функция расчета предлагаемой стоимости объекта на основе дохода предыдущих месяцев. 
+-- Функция расчета предлагаемой стоимости объекта на основе дохода предыдущих месяцев.
+
 USE cd;
 
-DELIMITER $$
+DELIMITER &&
 
--- TODO: уточнить задание.
-
-DROP PROCEDURE IF EXISTS getNewCostPreviousBased $$
-CREATE FUNCTION getNewCostPreviousBased(facid INT, currentCost DECIMAL(10, 2), percent DECIMAL(10,2)) RETURNS DECIMAL(10, 2)
+DROP FUNCTION IF EXISTS getDiffPayback &&
+CREATE FUNCTION getDiffPayback(facid INT, fraction FLOAT, curtime TIMESTAMP)
+    RETURNS DECIMAL(10, 2)
     READS SQL DATA
     NOT DETERMINISTIC
 BEGIN
-    DECLARE newCost DECIMAL(10, 2);
+    DECLARE income DECIMAL(10, 2);
+    DECLARE maintenance DECIMAL(10, 2);
+    DECLARE profit DECIMAL(10, 2);
+    DECLARE init_cost DECIMAL(10, 2);
 
-    SELECT () INTO newCost
-    FROM payments;
+    SELECT SUM(p.payment) INTO income
+    FROM payments AS p
+    JOIN bookings AS b ON b.bookid = p.bookid
+    JOIN facilities AS f ON b.facid = f.facid
+    WHERE facid = b.facid AND b.starttime <= curtime
+    GROUP BY b.facid;
 
-    -- SELECT b.facid, f.facility,
-    -- SUM(p.payment) - f.monthlymaintenance AS income
-    -- FROM bookings AS b
-    -- JOIN payments AS p ON b.bookid = p.bookid
-    -- JOIN facilities AS f ON b.facid = f.facid
-    -- WHERE DATE_FORMAT(starttime, '%y %m') = DATE_FORMAT(whichDate, '%y %m')
-    -- GROUP BY b.facid
-    -- ORDER BY b.facid;
+    IF income IS NULL THEN
+        RETURN 1;
+    END IF;
 
-    RETURN newCost;
-END $$
+    -- Расчет расходов за все время с точностью до дня
+    SELECT f.monthlymaintenance * (MONTH(curtime) - MONTH(MIN(b.starttime)) + 1) INTO maintenance
+    FROM facilities AS f
+    JOIN bookings AS b ON b.facid = f.facid
+    WHERE facid = b.facid
+    GROUP BY b.facid;
+
+    -- Начальная стоимость
+    SELECT f.initialoutlay INTO init_cost
+    FROM facilities AS f
+    WHERE facid = f.facid
+    LIMIT 1;
+
+    -- Прибыль
+    SET profit = (income - maintenance);
+
+    -- Проверка оккупаемости
+    IF profit <= 0 OR init_cost <= profit THEN
+        RETURN 1;
+    END IF;
+
+    -- Коэффициент стоимости аренды
+    RETURN ((1 / fraction) * (init_cost - profit) / init_cost * (1 - maintenance / income) + maintenance / income);
+END &&
 
 DELIMITER ;
 
--- SELECT
---     facid
--- ,   getNewCostPreviousBased(facid, , 0.1) as cost
--- FROM facilities;
+-- Сохранение данных в CSV-файл.
+SELECT getDiffPayback(2, 2, '2012-07-31-23:59:59')
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output.csv' 
+    FIELDS ENCLOSED BY '"' 
+    TERMINATED BY ';' 
+    ESCAPED BY '"' 
+    LINES TERMINATED BY '\r\n';
